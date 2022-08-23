80d91353b00dd9eb831951d235e4f074ce549bc4
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-23 10:49:32 -0400

    Added contact page with an email forwarding service and live map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
                 </Route>
             </Routes>
         </>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,107 @@
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const Contact = () => {
+
+    const [letterClass] = useState('text-animate')
+
+
+    const refForm = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_m9juybc',
+            'template_2a0hlzd',
+            refForm.current,
+            '7F_SCdRCBG2x0RWSI'
+        ).then(
+            () => {
+                alert('Message send successfully!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Message failed to send, please try again.')
+            }
+        )
+    }
+
+
+    return (
+        <>
+
+            <div className='container contact-page'>
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15} />
+                    </h1>
+                    <p>
+                        I am currently looking for employment in the tech industry -
+                        I am especially interested in fields regarding development.
+                        If you have any requests or questions, please contact me here.
+                    </p>
+                    <div className="contact-form">
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className="half">
+                                    <input type="text" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input type="text" name="subject" placeholder="Subject" required />
+                                </li>
+                                <li>
+                                    <textarea name="message" placeholder="Message" required />
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className='info-map'>
+                    Jacob Berendsohn,
+                    <br />
+                    New York / Connecticut
+                    <br />
+                    <span>berendsohnjacob@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[41.40180528627441, -73.45860017296891]} zoom={10}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[41.40180528627441, -73.45860017296891]}>
+                            <Popup> My Area </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+
+            </div>
+
+            <Loader type="ball-zig-zag" />
+        </>
+    )
+}
+
+export default Contact;
