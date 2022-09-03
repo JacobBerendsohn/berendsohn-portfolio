367e9785670938897699a60ce3088c6057a0bd42
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-03 11:37:27 -0400

    Added boilerplate code and proper divs to format project rows in portfolio tab

---
+++ src/components/ProjectRow/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='project-row'>
+        <div className='project-row-right'>
+            <img className='project-image' />
+            <div className='project-over'>
+
+            </div>
+        </div>
+        <div className='project-row-left'>
+            <div className='project-description'>
+                <p className='description-paragraph'>
+
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
